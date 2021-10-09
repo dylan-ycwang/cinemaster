9eb515e3bc1f87cdc5c09fd273ff7fa7992ec4e8
Dylan Wang <wangvc2017@gmail.com>
2021-10-09 12:20:52 +0800

    added BaaS configurations

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // app.js
 App({
   onLaunch() {
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login,
+     wx.getUserInfo,
+     wx.requestPayment)
+
+    wx.BaaS.init('0a3ec5fa38f2a1c6a950')
+    wx.BaaS.auth.loginWithWechat() // 静默登录
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -15,5 +23,5 @@ App({
   },
   globalData: {
     userInfo: null
-  }
+  }, 
 })
